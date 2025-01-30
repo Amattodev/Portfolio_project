016d928217b9c1026665b9ae098e9ec9885e719b
Amattodev <takuhuyu44@gmail.com>
2025-01-30 16:47:16 +0900

    サーバー設定

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,22 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const usersRouter = require('./routes/users');
+const portfoliosRouter = require('./routes/portfolios');
+
 //expressの設定
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//ルートの設定
+app.use('/api/portfolios', portfoliosRouter);
+app.use('/api/users', usersRouter);
+
 // MongoDB接続
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
 // テスト用のルート
 app.get('/', (req, res) => {
