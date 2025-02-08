99db9145d2008511305836c6fb6da3cf7b7ac86e
Amattodev <takuhuyu44@gmail.com>
2025-02-09 04:22:41 +0900

    クライアントAPIを作成（検索機能）

--- frontend/src/api/portfolios.js
+++ frontend/src/api/portfolios.js
@@ -11,3 +11,8 @@ export async function getPortfolioById(id) {
 export async function createPortfolio(portfolioData) {
     return client.post('/portfolios', portfolioData)
 }
+
+export async function searchPortfolios(search = '') {
+    const queryParams = search ? `?search=${encodeURIComponent(search)}` : '';
+    return client.get(`/portfolios${queryParams}`);
+}
