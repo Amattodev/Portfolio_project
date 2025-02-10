94cdc58563fadaf40abfca5219b3b0ee59fa7014
Amattodev <takuhuyu44@gmail.com>
2025-02-10 23:06:14 +0900

    いいねとポートフォリオカウントのAPIモデル作成

--- backend/models/user.js
+++ backend/models/user.js
@@ -16,10 +16,30 @@ const userSchema = new mongoose.Schema({
     photoURL: String,
     twitter: String,
     github: String,
+    portfolioCount: {
+        type: Number,
+        default: 0
+    },
+    totalLikes: {
+        type: Number,
+        default: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
     }
-})
+});
+
+userSchema.methods.updatePortfolioCount =  async function() {
+    const count = await mongoose.model('Portfolio').countDocuments({user: this._id});
+    this.portfolioCount = count;
+    return this.save();
+};
+
+userSchema.methods.updateTotalLikes = async function() {
+    const portfolios = await mongoose.model('Portfolio').find({ user: this._id });
+    this.totalLikes = portfolios.reduce((total, portfolio) => total + portfolio.likes.length, 0);
+    return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
